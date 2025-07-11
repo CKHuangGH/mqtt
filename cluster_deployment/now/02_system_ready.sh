1b75d40152f2f5a6d780a21f8991802adc4a7945
CKHuangGH <chihkaihuang1@gmail.com>
2025-07-11 15:06:38 +0200

    Update 02_system_ready.sh

--- cluster_deployment/now/02_system_ready.sh
+++ cluster_deployment/now/02_system_ready.sh
@@ -23,27 +23,4 @@ ssh -o StrictHostKeyChecking=no root@$j chmod 777 -R /root/mqtt/
 i=$((i+1))
 done
 
-scp -r /home/chuang/addon root@$manage:/root/
-scp -r /home/chuang/karmada_package root@$manage:/root/
-scp -r /home/chuang/images_system root@$manage:/root/
-scp -r /home/chuang/images_google root@$manage:/root/
-# scp -r /home/chuang/ocm_package root@$manage:/root/
-
-# scenario1
-scp node_list root@$manage:/root/sec2025/federation_framework/scenario1/ocm/node_list
-scp node_list root@$manage:/root/sec2025/federation_framework/scenario1/karmada-pull/node_list
-scp node_list root@$manage:/root/sec2025/federation_framework/scenario1/karmada-push/node_list
-scp node_list root@$manage:/root/sec2025/federation_framework/scenario1/kubefed/node_list
-
-# scenario2
-scp node_list root@$manage:/root/sec2025/federation_framework/scenario2/ocm/node_list
-scp node_list root@$manage:/root/sec2025/federation_framework/scenario2/karmada-pull/node_list
-scp node_list root@$manage:/root/sec2025/federation_framework/scenario2/karmada-push/node_list
-scp node_list root@$manage:/root/sec2025/federation_framework/scenario2/kubefed/node_list
-
-# scenario3
-scp node_list root@$manage:/root/sec2025/federation_framework/scenario3/ocm/node_list
-scp node_list root@$manage:/root/sec2025/federation_framework/scenario3/karmada-pull/node_list
-scp node_list root@$manage:/root/sec2025/federation_framework/scenario3/karmada-push/node_list
-scp node_list root@$manage:/root/sec2025/federation_framework/scenario3/kubefed/node_list
-echo "management node is $manage"
+scp node_list root@$manage:/root/mqtt/node_list
