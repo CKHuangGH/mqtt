6d9bf1b81d26a6327e9aea1b7670b36d8c91e2aa
CKHuangGH <chihkaihuang1@gmail.com>
2025-07-23 18:06:25 +0200

    Update deploy.sh

--- package/emqx/deploy.sh
+++ package/emqx/deploy.sh
@@ -11,7 +11,7 @@ helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manag
 # Install the EMQX
 helm install --version 5.8.7 emqx emqx/emqx --namespace emqx --create-namespace 
 
-helm install --version 2.1.2 influxdb2 influxdata/influxdb2 --namespace influxdb --create-namespace --set persistence.enabled=true --set persistence.storageClass=local-path --set persistence.size=30Gi --set config.authEnabled=false
+helm install --version 2.1.2 influxdb2 influxdata/influxdb2 --namespace influxdb --create-namespace --set persistence.enabled=true --set persistence.storageClass=local-path --set persistence.size=30Gi --set config.authEnabled=false --set service.type=NodePort --set service.nodePort=32086
 
 # ---- InfluxDB 設定 ----
 NAMESPACE="influxdb"  # InfluxDB 的 namespace
@@ -33,5 +33,4 @@ helm install --version 75.9.0 prometheus-community/kube-prometheus-stack \
   --set prometheus.prometheusSpec.resources.requests.cpu="1000m" \
   --set prometheus.prometheusSpec.resources.requests.memory="1024Mi" \
   --set prometheus.prometheusSpec.remoteWrite[0].url="${INFLUX_HOST}/api/v2/write?org=${ORG}&bucket=${BUCKET}&precision=s" \
-  --set prometheus.prometheusSpec.retention="5m" \
-  --set prometheus.prometheusSpec.storageSpec.volumeClaimTemplate.spec.resources.requests.storage="1Gi"
+  --set prometheus.prometheusSpec.retention="5m"
