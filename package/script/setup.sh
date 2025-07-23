ee6f9117cabc8c8f5c66d6008988fa2ceea71425
CKHuangGH <chihkaihuang1@gmail.com>
2025-07-23 16:07:22 +0200

    ww

--- package/script/setup.sh
+++ package/script/setup.sh
@@ -30,6 +30,3 @@ kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 helm repo update
 helm install cilium cilium/cilium --version 1.17.5 --wait --wait-for-jobs --namespace kube-system --set operator.replicas=1
 sleep 30
-
-kubectl create ns monitoring
-helm install --version 75.9.0 prometheus-community/kube-prometheus-stack --generate-name --set grafana.enabled=false --set alertmanager.enabled=false --set prometheus.service.type=NodePort --set prometheus.prometheusSpec.scrapeInterval="5s" --set prometheus.prometheusSpec.enableAdminAPI=true --namespace monitoring --set prometheus.prometheusSpec.resources.requests.cpu="1000m" --set prometheus.prometheusSpec.resources.requests.memory="1024Mi"
