40445680a6e10915214cd34e84da4206850dc158
CKHuangGH <chihkaihuang1@gmail.com>
2025-07-24 13:20:48 +0200

    Update setup.sh

--- package/script/setup.sh
+++ package/script/setup.sh
@@ -30,3 +30,23 @@ kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 helm repo update
 helm install cilium cilium/cilium --version 1.17.5 --wait --wait-for-jobs --namespace kube-system --set operator.replicas=1
 sleep 30
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+cd /root/bench_ctrl
+
+docker compose build
