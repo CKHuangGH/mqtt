d50432604be659c458628933957125a8af7daa28
CKHuangGH <chihkaihuang1@gmail.com>
2025-08-28 18:21:52 +0200

    ww

--- package/test2/01-deploy.sh
+++ package/test2/01-deploy.sh
@@ -1,2 +1,1 @@
-kubectl create configmap mosquitto-config  --from-file=mosquitto.conf=/root/ps_bench/ps_bench/mosquitto.conf
-kubectl apply -f k8s/
+kubectl create configmap mosquitto-config  --from-file=mosquitto.conf=/root/ps-bench/ps_bench/mosquitto.conf
