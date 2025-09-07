99e0650d88c322a62b3abe38b9c53275bf35ac76
CKHuang-public <chihkaihuang1@gmail.com>
2025-09-07 02:03:13 +0200

    wwr

--- package/exps/01_protocolsuite/08_protocolsuite_smart_home_mqttv5_emqx/pull.sh
+++ package/exps/01_protocolsuite/08_protocolsuite_smart_home_mqttv5_emqx/pull.sh
@@ -29,7 +29,7 @@ kubectl get pvc -o wide >> results/cluster_info.txt
 
 sleep 5
 
-ssh -o StrictHostKeyChecking=no chuang@172.16.111.106 "mkdir -p /home/chuang/protocolsuite_smart_factory_mqttv5_emqx/"
-scp -o StrictHostKeyChecking=no -r ./results chuang@172.16.111.106:/home/chuang/protocolsuite_smart_factory_mqttv5_emqx/$time
+ssh -o StrictHostKeyChecking=no chuang@172.16.111.106 "mkdir -p /home/chuang/protocolsuite_smart_home_mqttv5_emqx/"
+scp -o StrictHostKeyChecking=no -r ./results chuang@172.16.111.106:/home/chuang/protocolsuite_smart_home_mqttv5_emqx/$time
 
 rm -rf results/
