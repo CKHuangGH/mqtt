f9fd014797c75b27a694aa999c0332bed33f6866
CKHuang-public <chihkaihuang1@gmail.com>
2025-12-04 23:32:58 +0100

    ww

--- package/exps/5_runners/rebuttal/02_scalabilitysuite_smart_city_dds/01_run.sh
+++ package/exps/5_runners/rebuttal/02_scalabilitysuite_smart_city_dds/01_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for (( times=0; times<3; times++ )); do
+for (( times=0; times<1; times++ )); do
     kubectl apply -f runner-service.yaml
     sleep 2
     kubectl apply -f runner1-deployment.yaml
