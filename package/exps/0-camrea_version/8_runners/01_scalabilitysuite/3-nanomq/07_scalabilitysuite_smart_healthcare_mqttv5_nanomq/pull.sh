27f5d209b8ef4cc0491859b04fea6866d7bd6191
CKHuang-public <chihkaihuang1@gmail.com>
2026-01-14 00:51:53 +0100

    ee

--- package/exps/0-camrea_version/8_runners/01_scalabilitysuite/3-nanomq/07_scalabilitysuite_smart_healthcare_mqttv5_nanomq/pull.sh
+++ package/exps/0-camrea_version/8_runners/01_scalabilitysuite/3-nanomq/07_scalabilitysuite_smart_healthcare_mqttv5_nanomq/pull.sh
@@ -11,7 +11,9 @@ for i in 1 2 3 4 5 6 7 8; do
   # kubectl cp -c runnermqtt${i} "$pod":/app/out     "results/"
   kubectl cp -c runnermqtt${i} "$pod":/app/results "results/"
 done
-
+broker=$(kubectl get pods -o name | grep "^pod/nanomq-" | head -n1 | cut -d/ -f2)
+mkdir -p results/brokerlog
+kubectl cp -c nanomq "$broker":/logs "results/brokerlog"
 echo "==== kubectl get pod -o wide ====" >> results/cluster_info.txt
 kubectl get pod -o wide >> results/cluster_info.txt
 
@@ -56,7 +58,8 @@ done < node_ip_workers
 sleep 5
 
 ssh -o StrictHostKeyChecking=no chuang@172.16.111.106 "mkdir -p /home/chuang/scalabilitysuite_smart_healthcare_mqttv5_nanomq/"
-scp -o StrictHostKeyChecking=no -r ./results chuang@172.16.111.106:/home/chuang/scalabilitysuite_smart_healthcare_mqttv5_nanomq/$time
+tar -I 'gzip -9' -cf results.tar.gz results/
+scp -o StrictHostKeyChecking=no results.tar.gz chuang@172.16.111.106:/home/chuang/scalabilitysuite_smart_healthcare_mqttv5_nanomq/$time
 ssh -o StrictHostKeyChecking=no chuang@172.16.111.106 "mkdir -p /home/chuang/scalabilitysuite_smart_healthcare_mqttv5_nanomq/$time/deployment_files/"
 scp -o StrictHostKeyChecking=no ./runner1-deployment.yaml chuang@172.16.111.106:/home/chuang/scalabilitysuite_smart_healthcare_mqttv5_nanomq/$time/deployment_files/runner1-deployment.yaml
 scp -o StrictHostKeyChecking=no ./runner2-deployment.yaml chuang@172.16.111.106:/home/chuang/scalabilitysuite_smart_healthcare_mqttv5_nanomq/$time/deployment_files/runner2-deployment.yaml
